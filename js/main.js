d167bd4af5c70ffd9a497f743585807c5aec2ac1
Igor Natan <igornatan4@gmail.com>
2022-12-28 20:29:18 -0300

    commit function assync buscaEndereco

---
+++ js/main.js
@@ -0,0 +1,7 @@
+async function buscaEndereco() {
+    const consultaCEP = await fetch('https://viacep.com.br/ws/01001000/json/')
+    const consultaCEPConvertida = await consultaCEP.json();
+    console.log(consultaCEPConvertida);
+}
+
+buscaEndereco();
