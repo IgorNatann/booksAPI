61f3495e6694a27df6b28120f556786b2f7b59fb
Igor Natan <igornatan4@gmail.com>
2022-12-28 21:32:07 -0300

    commit mensagem de erro

--- js/main.js
+++ js/main.js
@@ -1,15 +1,30 @@
 async function buscaEndereco(cep) {
+    const mensagemErro = document.getElementById('erro');
+    mensagemErro.innerHTML = "";
+
     try { // Try = TENTAR
-    const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
-    const consultaCEPConvertida = await consultaCEP.json();
-    if (consultaCEPConvertida.erro) {
-        throw Error('CEP não existente!')
-    }
-    console.log(consultaCEPConvertida);
-    return consultaCEPConvertida;
+        const consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        const consultaCEPConvertida = await consultaCEP.json();
+        if (consultaCEPConvertida.erro) {
+            throw Error('CEP não existente!')
+        } 
+        const cidade = document.getElementById('cidade');
+        const logradouro = document.getElementById('endereco');
+        const estado = document.getElementById('estado');
+        const bairro = document.getElementById('bairro');
+        
+        cidade.value = consultaCEPConvertida.localidade;
+        logradouro.value = consultaCEPConvertida.logradouro;
+        estado.value = consultaCEPConvertida.uf;
+        bairro.value = consultaCEPConvertida.bairro;
+
+        return consultaCEPConvertida;
     } catch (erro) { // Catch = PEGAR
-        console.log(erro);
+        mensagemErro.innerHTML = ` <p>CEP inválido. Tente novamente!</p> `
     }
 }
 
+const cep = document.getElementById('cep');
+cep.addEventListener("focusout", () => buscaEndereco(cep.value));
 
+// 01001000
